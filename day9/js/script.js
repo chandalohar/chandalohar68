b29e925778db8eda4e7635dea858ae4ecbb123c8
chandalohar <52570299+chandalohar@users.noreply.github.com>
2022-12-08 01:17:28 +0530

    Add files via upload

---
+++ day9/js/script.js
@@ -0,0 +1,165 @@
+//	Create a variable for employee and display data in UI.
+//alert('Employee');//display
+
+//2.	var x = ++num //num = 20
+var x = 20;
+++x;
+console.log(x);        //21
+
+//3.	var y = num++ //num = 10
+var y = 20;
+z = y++;
+console.log(z);        //20
+
+/*4.	var num = 20;
+var x = ++num;
+console.log(x);*/
+var num = 20;
+var x = ++num;         //21
+console.log(x);
+
+
+
+
+/*5.	var numOne = 10;
+var y = numOne++;
+console.log(y);
+console.log(numOne);
+ */
+var numOne = 10;
+var y = numOne++;
+console.log(y);   //10
+console.log(numOne); //11
+
+/*6.	var val = 23;
+var t = ++val;
+val++; 
+console.log(val)
+console.log(t);
+ */
+var val = 23;
+var t = ++val;
+val++; 
+console.log(val);//25
+console.log(t); //24
+
+
+/*7.	var numVal = 30;
+var u = --numVal;
+numVal++;
+console.log(u);
+console.log(numVal);
+ */
+var numVal = 30;
+var u = --numVal;
+numVal++;
+console.log(u); //29
+console.log(numVal); //30
+
+
+
+/*8.	var a = 40;
+var b = a++;
+
+b++;
+console.log(a);
+console.log(b);
+  */
+var a = 40;
+var b = a++;
+b++;
+console.log(a); //41          NOT UNDERSTOOD
+console.log(b);  //41
+ 
+
+
+
+
+/*9.	var f = 50;
+var g = f++;
+g--; //
+console.log(g);
+console.log(f);
+*/
+var f = 50;
+var g = f++;
+g--; //
+console.log(g); //49         NOT UNDERSTOOD
+console.log(f);  //51
+ 
+
+
+/*10.	var val = 10;
+val++;
+var h = --val;
+h++;
+console.log(h);
+console.log(val);
+*/
+var val = 10;
+val++;
+var h = --val;
+h++;
+console.log(h);    //11
+console.log(val);  //10
+ 
+
+/*11.	var num = 20;
+num++;
+var t = ++num;
+num++;
+--num
+console.log(num, t);
+ */
+var num = 20;
+num++;
+var t = ++num;
+num++;
+--num
+console.log(num, t);            //22,22
+
+/*12.	var num = 10;
+--num;
+var y = ++num + 10;
+--y;
+console.log(y);
+console.log(num);
+ */
+
+var num = 10;
+--num;
+var y = ++num + 10;
+--y;
+console.log(y); //19
+console.log(num); //10
+
+/*13.	var num = 30;
+++num;
+num++ - 10;
+console.log(num);
+ */
+
+var num = 30;
+++num;
+var y = num++ - 10;
+console.log(num);   //32
+console.log(y);   //21
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
